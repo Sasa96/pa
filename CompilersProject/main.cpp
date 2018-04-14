82c53b6be334cf8718d2ccb679e8fbb167fdbda3
MahmoudKhalil <mahmoud@localhost.localdomain>
2018-04-15 00:16:26 +0200

    Comment part

--- CompilersProject/main.cpp
+++ CompilersProject/main.cpp
@@ -6,11 +6,33 @@ int main()
 {
     freopen ("input.txt","r",stdin);
     char x;
+    string comment = "";
+    int caseNo;
     while(cin>>x)
     {
+        cout << "here" << endl;
+        if(x == ':')
+        {
+            caseNo = 2;
+        } else if(x == '{')
+        {
+            caseNo = 4;
+            cout << "Here" << endl;
+        }
 
+        switch(caseNo)
+        {
+            case 4:
+                comment += x;
+                while(cin >> x && x != '}')
+                {
+                    comment += x;
+                }
 
+                comment += x;
 
+                cout << comment << " : COMMENT" << endl;
+                break;
+        }
     }
-
 }
